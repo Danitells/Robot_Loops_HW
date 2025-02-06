1590f113f6a6e87226434a0f252f986d124a0e5b
DariaSalhalova <salhalovadaria@gmail.com>
2025-02-06 20:54:49 +0200

    hw arrays

---
+++ arrays.js
@@ -0,0 +1,27 @@
+
+const stringArray = ["apple", "banana", "cherry"];
+
+
+const numberArray = [1, 2, 3, 4, 5];
+
+
+const booleanArray = [true, false, true, false];
+
+
+const anyArray = ["text", 42, true, { key: "value" }];
+
+
+
+
+stringArray.forEach(item => console.log(item.toUpperCase()));
+
+
+const squaredNumbers = numberArray.map(num => num * num);
+console.log(squaredNumbers);
+
+
+booleanArray.forEach(bool => console.log(bool ? "YES" : "NO"));
+
+
+const transformedAnyArray = anyArray.map(item => typeof item);
+console.log(transformedAnyArray);
---
+++ loop.js
@@ -0,0 +1,12 @@
+function smallStepLoop() {
+    for (let i = 0; i < 10; i++) {
+        console.log(i);
+    }
+}
+function bigStepLoop() {
+    for (let i = 100; i >= 0; i -= 10) {
+        console.log(i);
+    }
+}
+smallStepLoop()
+bigStepLoop()
